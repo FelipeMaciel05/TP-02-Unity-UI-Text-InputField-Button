b1dcfb247ccc8960b087217c57f07a2cc8ce7286
FelipeMaciel05 <felimorita05@gmail.com>
2022-04-21 21:55:48 -0300

    Trabajo Finalizado

---
+++ Assets/Mayoriadeedad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Mayoriadeedad : MonoBehaviour
+{
+    public int Edad;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Edad >= 18)
+        {
+            
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            
+            Debug.Log("Es menor");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ValidarContraseña.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseñaCorrecta = "123";
+    string contraseñaUsuario;
+    public Text IngresoU;
+    public Text textomsj;
+    public GameObject cartelitomsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cartelitomsj.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void validarContraseña()
+    {
+        contraseñaUsuario = IngresoU.text;
+        if (contraseñaUsuario == contraseñaCorrecta)
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Bienvenido";
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            cartelitomsj.SetActive(true);
+            textomsj.text = "Contraseña Incorrecta";
+            Debug.Log("contraseña incorrecta");
+        }
+    }
+
+}
